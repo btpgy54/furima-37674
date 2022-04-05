50b4816181d237e50aabbc0813cb8668a15525ea
btpgy54 <t410y831@yahoo.co.jp>
2022-04-05 19:13:50 +0900

    ログインの有無で表示を変える

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
   def edit
   end
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      redirect_to index
     else
       render :edit
     end
