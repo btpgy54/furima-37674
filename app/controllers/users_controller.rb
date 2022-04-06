84d28e5bfe0b7c14c71502a06817f7ba9f41d88f
btpgy54 <t410y831@yahoo.co.jp>
2022-04-06 23:00:35 +0900

    新規登録後にトップページに遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,17 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-  end
-
-  def edit
-    @user = User.edit
-  end
-
   def show
     @user = User.find(params[:id])
+    @items = @user.items
   end
 
   def update
     if current_user.update(user_params)
-      redirect_to index
+      redirect_to root_path
     else
       render :edit
     end
   end
-
+  
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'items#index'
-  resources :users, only: [:edit, :update]
+  root to: 'items#index'
+  resources :items, except: :index
+  resources :users, only: :show
 end
