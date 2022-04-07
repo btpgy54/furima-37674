31d0a11adaececc9d4e456c8dda7ed710e5b5681
btpgy54 <t410y831@yahoo.co.jp>
2022-04-07 18:09:34 +0900

    itemsテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
+    @items = Item.all
   end
 
-  # def show
-  # end
+  def new
+  end
+
+  def create
+    Item.create(content: params[:content])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :items, except: :index
   resources :users, only: [:index, :new, :show, :create, :edit, :update]
+  post 'items' , to: 'items#create'
 end
