f163472c548e07d76bdc05c40ffcfbc54f0884e5
btpgy54 <t410y831@yahoo.co.jp>
2022-04-07 17:12:38 +0900

    単体テストコードの重複箇所の削除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,47 +10,8 @@ RSpec.describe User, type: :model do
       it '全ての項目が入力されていれば登録できる' do
         expect(@user).to be_valid
       end
-
-      it 'passwordとpassword_confirmationが一致すれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'passwordが6文字以上であれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'passowordが128文字以内であれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'passwordが半角英数字であれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'emailが重複していなければ登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'emailに@が含まれていれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'last_nameが全角であれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'first_nameが全角であれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'last_name_kanaが全角カタカナであれば登録できる' do
-        expect(@user).to be_valid
-      end
-
-      it 'first_name_kanaが全角カタカナであれば登録できる' do
-        expect(@user).to be_valid
-      end
     end
+    
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
@@ -197,38 +158,6 @@ RSpec.describe User, type: :model do
         @user.email = 'testmail'
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
-      end
-      it 'passwordが空ではログインできない' do
-        @user.password = ''
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password can't be blank")
-      end
-      it 'passwordが5文字以下ではログインできない' do
-        @user.password = '00000'
-        @user.password_confirmation = '00000'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-      end
-      it 'passwordが129文字以上ではログインできない' do
-        @user.password = Faker::Internet.password(min_length: 129)
-        @user.password_confirmation = @user.password
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
-      end
-      it 'passwordが半角数字のみの場合はログインできない' do
-        @user.password = '00000'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid')
-      end
-      it 'passwordが半角英字のみの場合はログインできない' do
-        @user.password = 'aaaaa'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid')
-      end
-      it 'passwordが全角の場合はログインできない' do
-        @user.password = 'AAAAA'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is invalid')
       end
     end
   end
