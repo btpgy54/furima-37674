24fdd811cca3a29220763a175e661ea9a4dcd027
btpgy54 <t410y831@yahoo.co.jp>
2022-04-11 12:01:05 +0900

    マイグレーションファイルで重複したものの削除

--- db/migrate/20220407093547_create_items.rb
+++ db/migrate/20220407093547_create_items.rb
@@ -8,6 +8,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :shipping_fee_id, null: false
       t.integer :prefecture_id, null: false
       t.integer :shipping_date_id, null: false
+      t.integer :price, null: false
       t.references :user, foreign_key: true
       t.timestamps
     end
--- db/migrate/20220408025658_add_shipping_fee_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddShippingFeeToItems < ActiveRecord::Migration[6.0]
-  def change
-    #add_column :items, :shipping_fee_id, :integer
-  end
-end
