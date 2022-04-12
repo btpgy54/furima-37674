930f4bac3cb84f7170ee2598d1ddabbe3fbcd424
btpgy54 <t410y831@yahoo.co.jp>
2022-04-12 13:29:09 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-  #before_action :set_item, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  #before_action :set_item, only: [:new, :create, :edit, :update, :destroy]
+  #before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -13,10 +14,36 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to item_path(@item.user_id)
     else
       render :new
     end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to @item
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
