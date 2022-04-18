25984dbc1747820925aebf7f1f989dfb69911a94
btpgy54 <t410y831@yahoo.co.jp>
2022-04-18 11:26:46 +0900

    挙動確認で指摘があったので修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -5,7 +5,7 @@ class OrderDestination
   with_options presence: true do
     validates :token, presence: true
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid.' }
